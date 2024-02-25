8bc98d3e6fd9a653964d5b3b10de703a9074edf1
MoStAfAmAhMoUdAbDo <mosmahabd952@gmail.com>
2024-02-25 22:52:59 +0200

    make the getStarteredBtn have usableon pressed functin

--- lib/features/onboarding/widgets/get_start_btn.dart
+++ lib/features/onboarding/widgets/get_start_btn.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:new_corse_project/core/themes/app_colors.dart';
 import 'package:new_corse_project/core/themes/app_strings.dart';
 import 'package:new_corse_project/core/themes/custom_text_style.dart';
 
 class GetStartesButton extends StatelessWidget {
-  const GetStartesButton({super.key});
+  const GetStartesButton({super.key, this.onPressed});
+  final void Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
-      onPressed: () {},
+      onPressed: onPressed,
       minWidth: double.infinity,
       height: 52.h,
       color: AppColors.myBlue,
